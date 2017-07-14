7bb07f0b06f98aaa6291f5a451dea756d82a5009
hbt <ht@datascanning.dk>
2017-07-14 09:26:02 +0200

    Add project files.

---
+++ Misc_Junk_and_Experiments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc_Junk_and_Experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
